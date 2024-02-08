d91fce337ee4bb0f0c69eb4efea2cff1bdc8fab4
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2024-02-08 16:15:54 -0300

    update

--- ArrayOfIntValuesUsingLINQ/obj/Debug/net8.0/ArrayOfIntValuesUsingLINQ.AssemblyInfo.cs
+++ ArrayOfIntValuesUsingLINQ/obj/Debug/net8.0/ArrayOfIntValuesUsingLINQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayOfIntValuesUsingLINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ef8040bb358e01e9b09fe9216f50510d00f64f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8faed84653f5a1be4bf2ff4df54b394833ebc554")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayOfIntValuesUsingLINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayOfIntValuesUsingLINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
